bc83cf42a5a9c0884cf47405dcc5bd1a0e46bf8b
gaetanse <gs99@hotmail.fr>
2018-11-29 11:24:03 +0100

    premier commit

---
+++ app/src/main/java/com/gaetanse/au_fil_de_l_eau_bears/MainActivity.java
@@ -0,0 +1,13 @@
+package com.gaetanse.au_fil_de_l_eau_bears;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
